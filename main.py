d0e821714ae39b0c226a5186ecc8c93c13367649
Kfont400 <89884205+Kfont400@users.noreply.github.com>
2022-04-18 00:42:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,5 @@
-############### Blackjack Project #####################
-
-#Difficulty Normal 😎: Use all Hints below to complete the project.
-#Difficulty Hard 🤔: Use only Hints 1, 2, 3 to complete the project.
-#Difficulty Extra Hard 😭: Only use Hints 1 & 2 to complete the project.
-#Difficulty Expert 🤯: Only use Hint 1 to complete the project.
-
-############### Our Blackjack House Rules #####################
-
-## The deck is unlimited in size. 
-## There are no jokers. 
-## The Jack/Queen/King all count as 10.
-## The the Ace can count as 11 or 1.
-## Use the following list as the deck of cards:
-## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-## The cards in the list have equal probability of being drawn.
-## Cards are not removed from the deck as they are drawn.
-## The computer is the dealer.
-
-##################### Hints #####################
 import random
-from replit import clear
+from os import system, name
 from art import logo
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10]
@@ -140,6 +120,13 @@ def dealer_hand():
   while len(dealer_hand) < 2:
     dealer_hand.append(pull_deck(dealer_hand_total))
   return dealer_hand
+
+
+def clear():
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
 
 
 #start game
